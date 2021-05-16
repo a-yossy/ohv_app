8755008d6ba2f71e3504df0ff0c9bce8bbdce743
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-05-16 21:08:48 +0900

    bio_pageのルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'top_pages#show'
-  get  'bio', to: 'bio_pages#show'
+  resource :bio_page, only: %i[show]
 end
