b02c3a2e6f68f892dc4aeed3a2086229e2e9413c
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-05-20 10:01:47 +0900

    VIDEOSページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'top_pages#show'
   resource :bio_page, only: %i[show]
+  resource :video_page, only: %i[show]
 end
