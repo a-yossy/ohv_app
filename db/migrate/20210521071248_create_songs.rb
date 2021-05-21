b6c31c13ef05a3cf991e303f2b0815d30410d412
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-05-21 23:17:52 +0900

    refactor: SongモデルからCdモデルへ外部キーを付けるため、songのmigrateを変更

--- db/migrate/20210521071248_create_songs.rb
+++ db/migrate/20210521071248_create_songs.rb
@@ -3,7 +3,7 @@ class CreateSongs < ActiveRecord::Migration[6.1]
     create_table :songs do |t|
       t.string :name
       t.integer :track_number
-      t.references :cd
+      t.integer :cd_id
 
       t.timestamps
     end
