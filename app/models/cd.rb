b8be3e6285cb918a3f11f2d2ad633a4865f065a0
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-05-22 00:13:57 +0900

    feat: Cdモデルにpresenceのvalidatesを追加

--- app/models/cd.rb
+++ app/models/cd.rb
@@ -1,4 +1,12 @@
 class Cd < ApplicationRecord
   has_many :songs
   default_scope -> { order(created_at: :desc) }
+  with_options presence: true do
+    validates :format
+    validates :title
+    validates :release_date
+    validates :price
+    validates :url
+    validates :picture
+  end
 end
