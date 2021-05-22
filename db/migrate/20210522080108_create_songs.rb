59664aa273544adc229533390d5673057264dbbb
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-05-22 17:42:48 +0900

    feat: SongモデルにNOT NULL制約設定のため、songのマイグレートファイルを変更

--- db/migrate/20210522080108_create_songs.rb
+++ db/migrate/20210522080108_create_songs.rb
@@ -1,9 +1,9 @@
 class CreateSongs < ActiveRecord::Migration[6.1]
   def change
     create_table :songs do |t|
-      t.string :name
-      t.integer :track_number
-      t.integer :cd_id
+      t.string :name, null: false
+      t.integer :track_number, nul: false
+      t.integer :cd_id, nul: false
 
       t.timestamps
     end
