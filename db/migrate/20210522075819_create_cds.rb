65e2b1fb28d365708046cc9265a7256364fcf3fe
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-05-22 17:44:16 +0900

    feat: CdモデルにNOT NULL制約設定のため、cdのマイグレートファイルを変更

--- db/migrate/20210522075819_create_cds.rb
+++ db/migrate/20210522075819_create_cds.rb
@@ -1,12 +1,12 @@
 class CreateCds < ActiveRecord::Migration[6.1]
   def change
     create_table :cds do |t|
-      t.string :format
-      t.string :picture
-      t.string :title
-      t.string :release_date
-      t.string :price
-      t.string :url
+      t.string :format, null: false
+      t.string :picture, null: false
+      t.string :title, null: false
+      t.string :release_date, null: false
+      t.string :price, null: false
+      t.string :url, null: false
 
       t.timestamps
     end
