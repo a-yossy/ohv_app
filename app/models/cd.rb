153a088d1165cb3dc79d0351128c5f15ee5583e1
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-05-22 17:29:55 +0900

    feat: Cdモデル、Songモデルにvalidatesを追加

--- app/models/cd.rb
+++ app/models/cd.rb
@@ -1,3 +1,9 @@
 class Cd < ApplicationRecord
   has_many :songs
+  validates :format, presence: true
+  validates :picture, presence: true
+  validates :title, presence: true
+  validates :release_date, presence: true
+  validates :picture, presence: true
+  validates :url, presence: true, length: { maximum: 2000 }
 end
