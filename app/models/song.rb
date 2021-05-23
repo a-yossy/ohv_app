5d6fd6aa47518450732c50cd16c3660cdb5d72da
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-05-24 00:09:38 +0900

    chore: 曲順でSongモデルを使うため、Songモデルを曲順で昇順に変更

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ApplicationRecord
+  default_scope -> { order(track_number: :asc) }
   validates :name, presence: true
   validates :track_number, presence: true, numericality: { only_integer: true }
   validates :cd_format, presence: true
