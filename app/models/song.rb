698086d82efceabd468378a3706b2494f10ed14b
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-06-04 22:27:39 +0900

    feat: track_numberに1以上の制約をつけるため、Songモデルを修正

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ApplicationRecord
   scope :recording_order, -> { order(track_number: :asc) }
   validates :name, presence: true
-  validates :track_number, presence: true, numericality: { only_integer: true }
+  validates :track_number, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
   validates :cd_id, presence: true
 end
