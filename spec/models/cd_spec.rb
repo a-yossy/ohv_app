f0761e8b495abfac675da9ca8d83e238e82fd55c
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-06-04 14:32:01 +0900

    Cdモデルのscopeのテスト作成

--- spec/models/cd_spec.rb
+++ spec/models/cd_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
+
 RSpec.describe Cd, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:format) }
     it { should validate_presence_of(:picture) }
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:release_date) }
@@ -10,7 +10,38 @@ RSpec.describe Cd, type: :model do
     it { should validate_length_of(:url).is_at_most(2000) }
   end
 
+  describe 'format' do
+    let(:cd) { build(:cd, format: format) }
+
+    subject do
+      cd.valid?
+      cd.errors
+    end
+
+    context 'with blank format' do
+      let(:format) { '' }
+
+      it { is_expected.to be_of_kind(:format, :blank) }
+    end
+
+    context 'with not blank format' do
+      let(:format) { 'format' }
+
+      it { is_expected.not_to be_of_kind(:format, :blank) }
+    end
+  end
+
+
   describe 'associations' do
     it { should have_many(:songs) }
+  end
+
+  describe "recently_released_at" do
+    let!(:new_cd) { create(:cd, release_date: "2021-01-02") }
+    let!(:old_cd) { create(:cd, release_date: "2021-01-01") }
+
+    it do
+      expect(Cd.recently_released_at.to_a).to eq [new_cd, old_cd]
+    end
   end
 end
