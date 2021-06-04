5498782626bfd2dd61aa3bdf4ac9b2bdb91b887c
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-06-04 23:00:09 +0900

    docs: Songのテストモデルをformatからnameに修正

--- spec/factories/songs.rb
+++ spec/factories/songs.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :song do
-    sequence(:name) { |i| "format#{i}" }
+    sequence(:name) { |i| "name#{i}" }
     sequence(:track_number) { |i| i }
     cd_id { create(:cd).id }
   end
