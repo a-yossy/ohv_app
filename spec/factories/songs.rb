0e220ec050ed7e99e54472fb15c7ae942d535279
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-06-04 22:26:07 +0900

    外部キー制約をテストで使用するため、SongモデルのFactoryBotを修正

--- spec/factories/songs.rb
+++ spec/factories/songs.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :song do
     sequence(:name) { |i| "format#{i}" }
     sequence(:track_number) { |i| i }
-    cd_id { FactoryBot.create(:cd).id }
+    cd_id { create(:cd).id }
   end
 end
