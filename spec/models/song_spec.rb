577454588fb2b257360bad7a7fdf9d2f432193b5
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-06-05 01:13:49 +0900

    test: track_numberの一意性をテストするため、song_specを修正

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -10,6 +10,31 @@ RSpec.describe Song, type: :model do
                 is_greater_than_or_equal_to(1)  }
   end
 
+  describe 'track_number' do
+    describe 'uniqueness' do
+      let(:cd) { create(:cd) }
+      let(:song) { build(:song, cd_id: cd.id, track_number: track_number) }
+
+      subject do
+        song.valid?
+        song.errors
+      end
+
+      before { create(:song, cd_id: cd.id, track_number: 1) }
+      context 'when track number is unique' do
+        let(:track_number) { 2 }
+
+        it { is_expected.not_to be_of_kind(:track_number, :taken) }
+      end
+
+      context 'when track number is not unique' do
+        let(:track_number) { 1 }
+
+        it { is_expected.to be_of_kind(:track_number, :taken) }
+      end
+    end
+  end
+
   describe 'recording_order' do
     let!(:cd) { create(:cd) }
     let!(:first_track_number_song) { create(:song, cd_id: cd.id, track_number: 1) }
