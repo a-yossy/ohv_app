e48284188ac2d1f3cdd6b0c26eb0398681f7ad45
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-06-05 18:02:08 +0900

    test: informationモデルのfactory botを修正

--- spec/factories/informations.rb
+++ spec/factories/informations.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :information do
     sequence(:title) { |i| "title#{i}" }
     sequence(:picture) { |i| "picture#{i}.jpg" }
-    sequence(:announcement_date) { |i| "present#{i}" }
+    sequence(:announcement_date) { |i| Date.current + i.days }
     sequence(:content) { |i| "content#{i}" }
     priority { 0 }
   end
