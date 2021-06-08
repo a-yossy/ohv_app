e352d00c4ae0923775ba9cf245429256c993cbd3
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-06-08 14:24:00 +0900

    test: top_pagesのdescribeをshowから#showに変更

--- spec/requests/top_pages_spec.rb
+++ spec/requests/top_pages_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 RSpec.describe TopPagesController, type: :request do
-  describe "show" do
+  describe "#show" do
     it do
       get root_path
       expect(response).to have_http_status(200)
