d4a2f663218fec22f2c56dc386b9121452189b43
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-06-09 19:34:32 +0900

    test: contact_pages_controllerをテストするため、contact_pages_specを追加

---
+++ spec/requests/contact_pages_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ContactPagesController, type: :request do
+  describe "#show" do
+    it do
+      get contact_page_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
