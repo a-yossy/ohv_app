fa95151a69140a717440ce4e180c051b3cb79d0c
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-06-18 00:29:22 +0900

    test: mailerのプレビューで食器モデルを使用するため、contact_mailer_previewを修正

--- spec/mailers/previews/contact_mailer_preview.rb
+++ spec/mailers/previews/contact_mailer_preview.rb
@@ -5,14 +5,18 @@ class ContactMailerPreview < ActionMailer::Preview
   # Preview this email at
   # http://localhost:3000/rails/mailers/contact_mailer/confirm_inquiry
   def confirm_inquiry
-    contact = Contact.first
+    contact = Contact.new(name: "name",
+                          email: "email@example.com",
+                          content: "content")
     ContactMailer.confirm_inquiry(contact)
   end
 
   # Preview this email at
   # http://localhost:3000/rails/mailers/contact_mailer/inquire_to_admin
   def inquire_to_admin
-    contact = Contact.first
+    contact = Contact.new(name: "name",
+                          email: "email@example.com",
+                          content: "content")
     ContactMailer.inquire_to_admin(contact)
   end
 end
