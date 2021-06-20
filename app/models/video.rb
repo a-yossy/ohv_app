0f48cc454a22b46c25ec81e293c260772f8645e7
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-06-20 18:09:12 +0900

    feat: Videoモデルがrelease_dateの降順で表示されるよう修正

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,5 @@
 class Video < ApplicationRecord
+  scope :recently_released_at, -> { order(release_date: :desc) }
   validates :title, presence: true
   validates :url, presence: true, length: { maximum: 2000 }
   validates :release_date, presence: true
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe Video, type: :model do
     it { should validate_presence_of(:release_date) }
     it { should validate_length_of(:url).is_at_most(2000) }
   end
+
+  describe "recently_released_at" do
+    let!(:new_video) { create(:video, release_date: "2021-01-02") }
+    let!(:old_video) { create(:video, release_date: "2021-01-01") }
+
+    it do
+      expect(described_class.recently_released_at.to_a).to eq [new_video, old_video]
+    end
+  end
 end
