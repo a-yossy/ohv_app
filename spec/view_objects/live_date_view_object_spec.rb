ae4db41b9a5c219f752e11e53848818ed16d6e76
YuyaYoshioka <yuya3330930@yahoo.co.jp>
2021-06-26 02:33:48 +0900

    test: live_date_view_objectのテストを追加

---
+++ spec/view_objects/live_date_view_object_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe LiveDateViewObject, type: :model do
+  describe 'live_dates' do
+    before do
+      create(:live, start_time: '2021/01/01')
+      create(:live, start_time: '2021/02/01')
+      create(:live, start_time: '2021/02/02')
+    end
+
+    subject { described_class.live_dates }
+
+    it { is_expected.to eq [["all_live", -Float::INFINITY..Float::INFINITY], ["2021-02", Date.new(2021,2,1).in_time_zone.all_month], ["2021-01", Date.new(2021,1,1).in_time_zone.all_month]].to_set }
+  end
+end
