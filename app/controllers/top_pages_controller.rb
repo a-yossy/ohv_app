45e708bc827c2ee3485b26a96a643eadc8ac4a50
yoshioka atsuya <atsuya0930@yahoo.co.jp>
2021-06-27 02:46:02 +0900

    feat: TOPページにLATEST NEWSとツイッターのタイムラインを表示

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class TopPagesController < ApplicationController
-  def show; end
+  def show
+    @informations = Information.recently_announced_at.first(2)
+  end
 end
