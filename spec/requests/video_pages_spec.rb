28bd80a45bcac82118fa390b26155ea6e31c3ab0
atsuya02 <atsuya0930@yahoo.co.jp>
2021-07-12 15:25:38 +0900

    test: VIDEOページのupdateアクションのテスト作成

--- spec/requests/video_pages_spec.rb
+++ spec/requests/video_pages_spec.rb
@@ -73,4 +73,28 @@ RSpec.describe VideoPagesController, type: :request do
       end
     end
   end
+
+  describe "#update" do
+    let(:video) { create(:video, url: "https://www.google.co.jp/?client=safari&channel=mac_bm") }
+
+    context "when user logs in" do
+      let(:admin) { create(:admin) }
+      before { login_as(admin) }
+
+      it do
+        put video_page_path(video), params: { video: { url: "https://www.yahoo.co.jp" } }
+        expect(video.reload.url).to eq "https://www.yahoo.co.jp"
+        expect(request).to redirect_to video_pages_path
+        expect(flash[:success]).to eq I18n.t "video_pages.update.success"
+      end
+    end
+
+    context "when user does not log in" do
+      it do
+        put video_page_path(video), params: { video: { url: "https://www.yahoo.co.jp" } }
+        expect(video.reload.url).not_to eq "https://www.yahoo.co.jp"
+        expect(response).to redirect_to new_admin_session_path
+      end
+    end
+  end
 end
