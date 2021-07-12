8d70848859ae305842ad074df494c9fb0b20e4c7
atsuya02 <atsuya0930@yahoo.co.jp>
2021-07-12 14:26:36 +0900

    feat: editとupdateアクションを作成するため、video_pages_controller.rbを修正

--- app/controllers/video_pages_controller.rb
+++ app/controllers/video_pages_controller.rb
@@ -20,6 +20,21 @@ class VideoPagesController < ApplicationController
     @videos = Video.recently_released_at
   end
 
+  def edit
+    @video = Video.find_by(id: params[:id])
+  end
+
+  def update
+    @video = Video.find_by(id: params[:id])
+    url = params[:video][:url]
+    @video.url = "https://www.youtube.com/embed/#{url.last(11)}"
+    if @video.update(video_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+
   private
 
   def video_params
