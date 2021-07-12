abd5b41de56cdf858f562e09fede35f617dfdd3d
atsuya02 <atsuya0930@yahoo.co.jp>
2021-07-12 15:00:01 +0900

    test: VIDEOページのnewアクションのテスト作成

--- spec/requests/video_pages_spec.rb
+++ spec/requests/video_pages_spec.rb
@@ -9,4 +9,23 @@ RSpec.describe VideoPagesController, type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "#new" do
+    context "when user logs in" do
+      let(:admin) { create(:admin) }
+      before { login_as(admin) }
+
+      it do
+        get new_video_page_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "when user does not log in" do
+      it do
+        get new_video_page_path
+        expect(response).to redirect_to new_admin_session_path
+      end
+    end
+  end
 end
