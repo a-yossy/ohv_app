2c77ebbcfdbefc9ade39a70add3678c8dca1ad30
atsuya02 <atsuya0930@yahoo.co.jp>
2021-07-12 13:39:12 +0900

    feat: newとcreateアクションを作成するため、video_pages_controller.rbを修正

--- app/controllers/video_pages_controller.rb
+++ app/controllers/video_pages_controller.rb
@@ -1,7 +1,28 @@
 # frozen_string_literal: true
 
 class VideoPagesController < ApplicationController
+  def new
+    @video = Video.new
+  end
+
+  def create
+    @video = Video.new(video_params)
+    url = params[:video][:url]
+    @video.url = "https://www.youtube.com/embed/#{url.last(11)}"
+    if @video.save
+      redirect_to action: :show
+    else
+      render :new
+    end
+  end
+
   def show
     @videos = Video.recently_released_at
+  end
+
+  private
+
+  def video_params
+    params.require(:video).permit(:title, :url, :release_date)
   end
 end
