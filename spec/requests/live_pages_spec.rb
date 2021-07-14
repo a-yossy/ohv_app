efd8d5eaecb39c7447a4d2e9829407a43fb72c56
atsuya02 <atsuya0930@yahoo.co.jp>
2021-07-15 08:08:49 +0900

    test: LIVEページのupdateアクションのテスト作成

--- spec/requests/live_pages_spec.rb
+++ spec/requests/live_pages_spec.rb
@@ -75,4 +75,27 @@ RSpec.describe LivePagesController, type: :request do
       end
     end
   end
+
+  describe "#update" do
+    let(:live) { create(:live, place: "old_place") }
+    subject { put live_page_path(live), params: { live: { place: "new_place" } } }
+
+    context "when user logs in" do
+      let(:admin) { create(:admin) }
+      before { login_as(admin) }
+
+      it do
+        expect { subject; live.reload }.to change(live, :place).from("old_place").to("new_place")
+        expect(response).to redirect_to live_pages_path
+        expect(flash[:success]).to eq I18n.t "live_pages.update.success"
+      end
+    end
+
+    context "when user does not log in" do
+      it do
+        expect { subject; live.reload }.not_to change(live, :place)
+        expect(response).to redirect_to new_admin_session_path
+      end
+    end
+  end
 end
