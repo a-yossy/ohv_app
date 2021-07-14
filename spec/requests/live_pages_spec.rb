e8559bc3d30ef1554ca457804dcce0b3daa8b2e8
atsuya02 <atsuya0930@yahoo.co.jp>
2021-07-15 07:30:22 +0900

    test: live_pagesのroutes修正のため、showからindexアクションのテストに変更

--- spec/requests/live_pages_spec.rb
+++ spec/requests/live_pages_spec.rb
@@ -3,9 +3,9 @@
 require "rails_helper"
 
 RSpec.describe LivePagesController, type: :request do
-  describe "#show" do
+  describe "#index" do
     it do
-      get live_page_path
+      get live_pages_path
       expect(response).to have_http_status(200)
     end
   end
