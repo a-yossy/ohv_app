fa0b280202167ac834b9f8023d4ded1fd4d3582e
atsuya02 <atsuya0930@yahoo.co.jp>
2021-07-31 19:44:52 +0900

    rubocopで修正

--- spec/requests/cd_pages_spec.rb
+++ spec/requests/cd_pages_spec.rb
@@ -41,7 +41,11 @@ RSpec.describe CdPagesController, type: :request do
   describe "#create" do
     let(:picture_path) { File.join(Rails.root, "spec/fixtures/test.jpg") }
     let(:picture) { Rack::Test::UploadedFile.new(picture_path) }
-    subject { post cd_pages_path, params: { cd_form_object: { format: "format", picture: picture, title: "title", release_date: "2021-01-01", price: 1000, url: "https://linkco.re/r1zyVcrS", songs: [ name_1: "name_1", track_number_1: 1, name_2: "name_2", track_number_2: 2 ], form_count: 2 } } }
+    subject {
+      post cd_pages_path,
+           params: { cd_form_object: { format: "format", picture: picture, title: "title", release_date: "2021-01-01", price: 1000, url: "https://linkco.re/r1zyVcrS",
+                                       songs: [name_1: "name_1", track_number_1: 1, name_2: "name_2", track_number_2: 2], form_count: 2 } }
+    }
 
     context "when user logs in" do
       let(:admin) { create(:admin) }
